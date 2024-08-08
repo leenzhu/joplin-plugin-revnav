07c0a1b38f97b03c78ef61235594ecb4e56d1120
leenzhu <i@leenzhu.com>
2024-08-08 09:46:27 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import joplin from 'api';
+import {ToolbarButtonLocation} from "api/types";
+
+joplin.plugins.register({
+	onStart: async function() {
+		// eslint-disable-next-line no-console
+		console.info('Hello revnav!');
+
+        await joplin.commands.register({
+            name: 'revealInNavigation',
+            label: 'Reveal current note in navgation',
+            iconName: 'fas fa-crosshairs',
+            execute: async () => {
+                const note = await joplin.workspace.selectedNote();
+                await joplin.commands.execute("openNote", note.id);
+            },
+        })
+
+        await joplin.views.toolbarButtons.create(
+            'revnav_reveal_current_note',
+            'revealInNavigation',
+            ToolbarButtonLocation.NoteToolbar
+        )
+	},
+});
